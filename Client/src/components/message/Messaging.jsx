9f30fc56b473368a6d043feed37fd7652c7b999c
Abhishek jangid <164181412+Abhishekkkk-15@users.noreply.github.com>
2024-12-22 15:08:50 +0530

    w-full for chating in smallscreen

--- Client/src/components/message/Messaging.jsx
+++ Client/src/components/message/Messaging.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
   const selectedUser = useSelector(state => state.message.selectedUser  ) 
  
   return (
-    <div className="h-screen bg-base-200 mb-12">
+    <div className="h-screen bg-base-200 mb-12 sm:w-full">
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
