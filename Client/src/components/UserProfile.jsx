b3170da9b201b2f42f24e0ea1752f9cb45b37fa7
Abhishek jangid <164181412+Abhishekkkk-15@users.noreply.github.com>
2024-12-22 14:50:30 +0530

    Changes in userProfile Component

--- Client/src/components/UserProfile.jsx
+++ Client/src/components/UserProfile.jsx
@@ -90,7 +90,7 @@ export default function UserProfile({ log }) {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 mt-2 w-40 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="absolute right-0 mt-2 bg-base w-40 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <Menu.Item>
                 {({ active }) => (
                   <Link to="/account"
